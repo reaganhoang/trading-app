c142cf1a940d2be8cd8c4154ceaacc0db900b213
Reagan Hoang <reagang4@users.noreply.github.com>
2020-05-29 08:04:13 -0700

    initial set up

---
+++ application.py
@@ -0,0 +1,44 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import mkdtemp
+from datetime import datetime
+
+from helpers import *
+
+# configure application
+app = Flask(__name__)
+
+# ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# custom filter
+app.jinja_env.filters["usd"] = usd
+
+# configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.route("/")
+@login_required
+def index():
+    u_id = (session.get("user_id"))
+
+    sum_data = db.execute("SELECT * FROM users WHERE id = :id", id=u_id)
+    trans_data = db.execute("SELECT * FROM trans WHERE user_id = :id", id=u_id)
+    port_data = db.execute("SELECT * FROM port WHERE user_id = :id", id=u_id)
+
+    #sum transactions and cash
+    
