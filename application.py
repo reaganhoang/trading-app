5b687e3f51c0bf07435cbbe903086aa2d23e878b
Reagan Hoang <reagang4@users.noreply.github.com>
2020-05-29 09:46:48 -0700

    more code

--- application.py
+++ application.py
@@ -161,4 +161,107 @@ def login():
 
     # else if user reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")       
+        return render_template("login.html")
+
+        @app.route("/logout")
+def logout():
+    """Log user out."""
+
+    # forget any user_id
+    session.clear()
+
+    # redirect user to login form
+    return redirect(url_for("login"))
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("quote"):
+            return apology("please enter a quote")
+
+        ticker = request.form.get("quote")
+
+        # query database for stock
+        stock_data = lookup(ticker)
+
+        if stock_data:
+            return render_template("quoted.html", ticker=ticker, name=stock_data["name"], price= stock_data["price"], symbol = stock_data["symbol"])
+        else:
+            return apology("Invalid stock name")
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("quote.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user."""
+    session.clear()
+
+    # if user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        # ensure password was submitted
+        elif not request.form.get("password") and request.form.get("pwconfirm"):
+            return apology("must provide password")
+
+        elif not request.form.get("password") == request.form.get("pwconfirm"):
+            return apology("passwords must match")
+
+        # query database for username
+        name = request.form.get("username")
+        pw = pwd_context.hash(request.form.get("password"))
+
+        if db.execute("SELECT * FROM users WHERE username = :name", name=name):
+                return apology("user already exists")
+        else:
+            db.execute("INSERT INTO users (username, hash) VALUES (:name, :hash)", name=name, hash=pw)
+
+        #login user automatically
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=name)
+
+        # remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect user to home page
+        return redirect(url_for("index"))
+
+    # else if user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")     
+
+    @app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock."""
+    if request.method == "POST":
+
+        u_id = (session.get("user_id"))
+
+        #validate inputs
+        if not request.form.get("ticker"):
+            return apology("please enter a ticker")
+        elif not request.form.get("quantity"):
+            return apology("please enter a quantity")
+        elif int(request.form.get("quantity")) < 1:
+            return apology("please enter a quantity greater than 0")
+
+        #see if user owns stock
+        stock = request.form.get("ticker").upper()
+        stock_data = lookup(stock)
+        if not stock_data:
+            return apology("Invalid stock name")
+        elif not db.execute("SELECT * FROM port WHERE user_id = :id AND symbol = :stock", id=u_id, stock=stock):
+            return apology("You don't own that stock, unfortunately")
+
+        # query yahoo for stock
+        price = stock_data["price"]
+        quantity = int(request.form.get("quantity"))
+        total = price * quantity  
